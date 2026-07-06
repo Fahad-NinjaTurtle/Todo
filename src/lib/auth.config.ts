b9b2bc936af81240d224f8e8cafe8331d17be619
Fahad Shahbaz <fahad@tecshield.io>
2026-07-06 18:25:01 +0500

    Fixing

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -4,7 +4,7 @@ export const authConfig = {
   secret: process.env.AUTH_SECRET,
   trustHost: true,
   session: { strategy: 'jwt' },
-  pages: { signIn: '/login' },
+  pages: { signIn: '/login' }, 
   providers: [],
   callbacks: {
     jwt({ token, user }) {
